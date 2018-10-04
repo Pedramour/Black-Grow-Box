ce14368a29555a8fcb28aab14f4e9019b28ea212
Pedram <Ped.ahmadi@gmail.com>
2018-10-04 19:51:42 +0330

    Add files via upload

---
+++ bgb.vue
@@ -0,0 +1,166 @@
+<script>
+// Libraries
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+
+// Helpers
+import colors from 'vuetify/es5/util/colors'
+
+Vue.use(Vuetify, {
+  theme: {
+    primary: colors.pink.accent3, // #E53935
+    secondary: colors.grey.darken3, // #FFCDD2
+    accent: colors.grey.darken4 // #3F51B5
+  }
+});
+
+
+</script>
+<script>
+
+
+
+    new Vue({
+		
+	
+
+      
+    
+     
+	
+
+		
+            show:true,
+            el: '#app',
+			
+			data: () => ({
+				
+	
+	
+	
+  }),
+ 
+  
+            data: () => ({
+				
+            pagination: {
+                sortBy: 'name'
+				
+            },
+            selected: [],
+            headers: [
+
+                { text: 'دکمه ها', value: 'buttons' },
+                { text: 'وضعیت', value: 'state' },
+                { text: 'تاریخ ارسال', value: 'date' },
+                { text: 'عنوان درخواست', value: 'requestTitle' },
+                { text: 'ردیف', value: 'rowNumber' }
+            ],
+            rowNumber: [
+                {
+                    value: false,
+                    buttons: '',
+                    state: 'تایید نشده',
+                    date: '1379/3/2',
+                    requestTitle: 'درخواست لورم اسپیوم',
+                    rowNumber: '1'
+                },
+                {
+                    value: false,
+                    buttons: 237,
+                    state: 'تایید نشده',
+                    date: '1379/3/2',
+                    requestTitle: 'درخواست لورم اسپیوم',
+                    rowNumber: '2'
+                },
+                {
+                    value: false,
+                    buttons: 262,
+                    state: 'تایید نشده',
+                    date: '1379/3/2',
+                    requestTitle: 'درخواست لورم اسپیوم',
+                    rowNumber: '3'
+                },
+                {
+                    value: false,
+                    buttons: 305,
+                    state: 'تایید نشده',
+                    date: '1379/3/2',
+                    requestTitle: 'درخواست لورم اسپیوم',
+                    rowNumber: '4'
+                },
+                {
+                    value: false,
+                    buttons: 356,
+                    state: 'تایید نشده',
+                    date: '1379/3/2',
+                    requestTitle: 'درخواست لورم اسپیوم',
+                    rowNumber: '5'
+                },
+                {
+                    value: false,
+                    buttons: 375,
+                    state: 'تایید نشده',
+                    date: '1379/3/2',
+                    requestTitle: 'درخواست لورم اسپیوم',
+                    rowNumber: '6'
+                },
+                {
+                    value: false,
+                    buttons: 392,
+                    state: 'تایید نشده',
+                    date: '1379/3/2',
+                    requestTitle: 'درخواست لورم اسپیوم',
+                    rowNumber: '7'
+                },
+                {
+                    value: false,
+                    buttons: 408,
+                    state: 'تایید نشده',
+                    date: '1379/3/2',
+                    requestTitle: 'درخواست لورم اسپیوم',
+                    rowNumber: '8'
+                },
+                {
+                    value: false,
+                    buttons: 452,
+                    state: 'تایید نشده',
+                    date: '1379/3/2',
+                    requestTitle: 'درخواست لورم اسپیوم',
+                    rowNumber: '9'
+                },
+                {
+                    value: false,
+                    buttons: 518,
+                    state: 'تایید نشده',
+                    date: '1379/3/2',
+                    requestTitle: 'درخواست لورم اسپیوم',
+                    rowNumber: '10'
+                }
+
+            ]
+        }),
+
+        methods: {
+        toggleAll (){
+            if (this.selected.length) this.selected = []
+            else this.selected = this.desserts.slice()
+        },
+        changeSort (column){
+            if (this.pagination.sortBy === column) {
+                this.pagination.descending = !this.pagination.descending
+            } else {
+                this.pagination.sortBy = column
+                this.pagination.descending = false
+            }
+        },
+        next (){
+            const active = parseInt(this.active)
+            this.active = (active < 2 ? active + 1 : 0)
+        },
+    }
+
+
+  
+    })
+</script>
